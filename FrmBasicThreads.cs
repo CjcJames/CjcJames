0dd588177b257aeb430a88c56533a68840053003
CjcJames <116178412+CjcJames@users.noreply.github.com>
2022-10-27 15:10:57 +0800

    Add files via upload

---
+++ FrmBasicThreads.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace FrmBasicThread
+{
+    public partial class Form1 : Form
+    {
+        private Thread ThreadA, ThreadB;
+        
+        MyThreadClass classthread = new MyThreadClass();
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btbRun_Click(object sender, EventArgs e)
+        {
+            ThreadA = new Thread(MyThreadClass.Thread1);
+            ThreadA.Name = "Thread A";
+            ThreadB = new Thread(MyThreadClass.Thread1);
+            ThreadB.Name = "Thread B";
+            ThreadA.Start();
+            ThreadB.Start();
+
+            lblThreadStats.Text = "-Threads is Running-";
+
+            ThreadA.Join();
+            ThreadB.Join();
+
+            lblThreadStats.Text = "-End of Threads-";
+
+        }
+    }
+}
